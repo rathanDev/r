491086d885adcdf2314426dd3053518c27f3ae4a
jana <rathan.kparam@gmail.com>
2020-07-04 07:27:40 +0800

    Learn vector operations

---
+++ bootcamp/018VectorOperations.R
@@ -0,0 +1,29 @@
+# Addition Subtraction Multiplication
+
+v1 <- c(1,2,3)
+v2 <- c(4,5,6)
+v1+v2
+
+v2-v1
+
+v2*v1
+
+v2/v1
+
+# Summation Mean StandardDeviation Max Min
+
+sum.of.vec <- sum(v1)
+sum.of.vec
+
+mean(v1)
+
+sd(v1)
+
+max(v1)
+
+min(v1)
+
+prod(v1)
+
+
+
