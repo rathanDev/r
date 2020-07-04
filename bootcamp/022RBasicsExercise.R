58f29a23e0667652aebb7821483e2373542b9dd0
jana <rathan.kparam@gmail.com>
2020-07-04 09:52:44 +0800

    Practice

---
+++ bootcamp/022RBasicsExercise.R
@@ -0,0 +1,25 @@
+# R basics exercise
+# /udemyR/R-Course-HTML-Notes/R-for-Data-Science-and-Machine-Learning/TrainingExercises/Exercises/RBasicsExercise.html
+
+2 ^ 5
+
+stock.prices <- c(23,27,23,21,34)
+stock.prices
+
+daysOfWeek <- c('Mon','Tue','Wed','Thu','Fri')
+
+names(stock.prices) <- daysOfWeek
+
+stock.prices
+
+
+mean(stock.prices)
+
+over.23 <- stock.prices > 23
+over.23
+
+stock.prices[over.23]
+
+max(stock.prices)
+
+
