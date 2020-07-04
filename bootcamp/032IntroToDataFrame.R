a4518b9db2cd451d3b13e301d99eea2321e6b1d6
jana <rathan.kparam@gmail.com>
2020-07-05 06:46:56 +0800

    Learn dataframe

---
+++ bootcamp/032IntroToDataFrame.R
@@ -0,0 +1,39 @@
+state.x77
+
+?state.x77
+
+USPersonalExpenditure
+
+women
+
+
+?women
+
+head(women)
+
+tail(women)
+
+
+# str - structure
+
+str(women)
+
+summary(women)
+
+str(state.x77)
+
+summary(state.x77)
+
+
+
+
+days <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri')
+temp <- c(21, 22, 23, 24, 25)
+rain <- c(T, T, F, F, T)
+
+data.frame(days, temp, rain)
+
+df <- data.frame(days, temp, rain)
+
+
+
