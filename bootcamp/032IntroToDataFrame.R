1cd0b6d8c3fa334d12820902bb4e9be09684957f
jana <rathan.kparam@gmail.com>
2020-07-05 18:02:54 +0800

    Learn dataframe indexing and selection

--- bootcamp/032IntroToDataFrame.R
+++ bootcamp/032IntroToDataFrame.R
@@ -36,4 +36,35 @@ data.frame(days, temp, rain)
 df <- data.frame(days, temp, rain)
 
 
+# 34. DataFrames - Selection and Indexing
+
+
+df
+
+df[1,]
+df[,1]
+
+df['rain']
+df[,'rain']
+
+df[1:4, c('days','temp')]
+
+df$days
+df$temp
+
+subset(df, subset = rain==T)
+subset(df, temp>23)
+
+?order
+sorted.temp <- order(df['temp'], decreasing = T)
+sorted.temp
+df[sorted.temp,]
+
+sorted.temp <- order(-df['temp'], decreasing = T)
+df[sorted.temp,]
+
+df[order(df$temp),]
+df[order(-df$temp),]
+
+
 
