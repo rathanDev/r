6badac6a3aa79b80319daa3de8fa0832f7e8591c
jana <rathan.kparam@gmail.com>
2020-07-25 20:48:51 +0800

    Practice dplyr more

--- bootcamp/066DplyrTrainingExercise.R
+++ bootcamp/066DplyrTrainingExercise.R
@@ -9,27 +9,51 @@ head(df)
 
 help("filter")
 
+
+
 res <- df %>% filter(mpg>20) %>% filter(cyl==6)
 res
 
+# filter(df, mpg>20, cyl==6)
+
+
+
 df %>% arrange(cyl, desc(wt)) 
+
+# arrange(df, cyl, desc(wt))
+
+
 
 # https://www.datanovia.com/en/lessons/select-data-frame-columns-in-r/
 
 df %>% select(mpg, hp)
 
+# select(df, mpg, hp)
+
+
+
 distinct(df, df$gear)
 
 df %>% distinct(gear)
 
+# distinct(select(df, gear))
+
+
+
+
 df['Performance'] <- df$hp / df$wt
 head(df)
 
-mean(df$mpg)
-
-df %>% filter(cyl==6) %>%  pull(hp) %>% mean
+# mutate(df, Performance2=hp/wt)
 
 
+
+
+# mean(df$mpg)
+
+# df %>% filter(cyl==6) %>%  pull(hp) %>% mean
+
+df %>% filter(cyl==6) %>% summarise(avg_hp = mean(hp))
 
 
 
